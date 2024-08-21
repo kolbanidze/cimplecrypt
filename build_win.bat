0e0d65b8a5213e9261cc7d98b7d5746d4a93c7e9
Kolbanidze <137103175+kolbanidze@users.noreply.github.com>
2024-08-21 22:26:35 +0300

    edited build_win.bat

--- build_win.bat
+++ build_win.bat
@@ -1,2 +1,2 @@
-cl.exe /Zi /nologo /DEBUG:NONE /Fo: "bin"\ /Fe:bin\decrypt.exe decrypt.c cargs.c -Iinclude /link /LIBPATH:include libsodium.lib
-cl.exe /Zi /nologo /DEBUG:NONE /Fo: "bin"\ /Fe:bin\encrypt.exe encrypt.c cargs.c -Iinclude /link /LIBPATH:include libsodium.lib
+cl.exe /Zi /nologo /DEBUG:NONE /Fo: "bin"\ /Fe:bin\windows_decrypt.exe decrypt.c cargs.c -Iinclude /link /LIBPATH:include libsodium.lib
+cl.exe /Zi /nologo /DEBUG:NONE /Fo: "bin"\ /Fe:bin\windows_encrypt.exe encrypt.c cargs.c -Iinclude /link /LIBPATH:include libsodium.lib
